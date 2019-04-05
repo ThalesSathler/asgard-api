9b4ea7b27faec8ca37cf5f1bd7e0670b6130d963
Dalton Barreto <daltonmatos@gmail.com>
2019-04-05 16:07:13 -0300

    Implementação simples do wrapper para Mesos API
    Por enquanto apenas com `get_agent_by_id()`.

--- conftest.py
+++ conftest.py
@@ -1,3 +1,4 @@
+import json
 import os
 
 os.environ["ENV"] = "TEST"
@@ -6,4 +7,13 @@ os.environ["TEST_DB_URL"] = os.getenv(
     "TEST_DB_URL", "postgresql://postgres@172.18.0.41/asgard"
 )
 
+MESOS_API_URLS = [
+    "http://172.18.0.11:5050",
+    "http://172.18.0.12:5050",
+    "http://172.18.0.13:5050",
+]
+
+os.environ["TEST_MESOS_API_URLS"] = os.getenv(
+    "TEST_MESOS_API_URLS", json.dumps(MESOS_API_URLS)
+)
 assert os.environ["ENV"] == "TEST"
