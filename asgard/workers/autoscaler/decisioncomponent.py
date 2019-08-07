a13da76107b54299ac4d945c3a3e153ea3d9a764
Thales Sathler <thales.lima@b2wdigital.com>
2019-08-07 15:06:54 -0300

    Implementação do decision component; acerto dos testes; inclusao da cpu e memoria alocada no scalble app

--- asgard/workers/autoscaler/decisioncomponent.py
+++ asgard/workers/autoscaler/decisioncomponent.py
@@ -1,16 +1,35 @@
 from abc import ABC, abstractmethod
 from typing import List
 
-from asgard.workers.models.app_stats import AppStats
+from asgard.workers.models.scalable_app import ScalableApp 
 from asgard.workers.models.scaling_decision import Decision
 
 
+THRESHOLD_MARGIN = 0.05
+
 class DecisionComponentInterface(ABC):
     @abstractmethod
-    def decide_scaling_actions(self, apps_stats: AppStats) -> Decision:
+    def decide_scaling_actions(self, apps_stats: List[ScalableApp]) -> List[Decision]:
         pass
 
 
 class DecisionComponent(DecisionComponentInterface):
-    def decide_scaling_actions(self, apps_stats: AppStats) -> Decision:
-        raise NotImplementedError
+    def decide_scaling_actions(self, apps_stats: List[ScalableApp]) -> List[Decision]:
+        decisions = []
+        for app in apps_stats:
+            decision = Decision(app.id)
+            deploy_decision = False
+            
+            if app.is_set_to_scale_cpu():
+                if app.app_stats.cpu_usage > app.cpu_threshold + THRESHOLD_MARGIN or app.app_stats.cpu_usage < app.cpu_threshold - THRESHOLD_MARGIN:
+                    decision.cpu = (app.app_stats.cpu_usage * app.cpu_allocated)/app.cpu_threshold
+                    deploy_decision = True
+            if app.is_set_to_scale_mem():
+                if app.app_stats.ram_usage > app.mem_threshold + THRESHOLD_MARGIN or app.app_stats.ram_usage < app.mem_threshold - THRESHOLD_MARGIN:
+                    decision.ram = (app.app_stats.ram_usage * app.mem_allocated)/app.mem_threshold
+                    deploy_decision = True
+            
+            if deploy_decision:
+                decisions.append(decision)
+
+        return decisions
--- asgard/workers/models/app_stats.py
+++ asgard/workers/models/app_stats.py
@@ -4,10 +4,6 @@ class AppStats:
             self,
             cpu_usage: float = None,
             ram_usage: float = None,
-            allocated_cpu: float = None,
-            allocated_ram: float = None
     ):
         self.cpu_usage = cpu_usage
         self.ram_usage = ram_usage
-        self.allocated_cpu = allocated_cpu
-        self.allocated_ram = allocated_ram
