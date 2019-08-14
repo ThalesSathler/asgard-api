f5f9f62278fa06088b2df9847be30f3dc917a003
Dalton Barreto <daltonmatos@gmail.com>
2019-08-14 14:02:35 -0300

    Migrando o restante do código para usar HttpClient

--- asgard/backends/mesos/models/agent.py
+++ asgard/backends/mesos/models/agent.py
@@ -1,12 +1,14 @@
 from collections import defaultdict
 from decimal import Decimal
-from typing import Any, Dict, List, Optional, Set, Union
+from typing import Dict, List, Set
 
 from asgard.backends.mesos.models.app import MesosApp
 from asgard.backends.mesos.models.task import MesosTask
-from asgard.http.client import http_client
+from asgard.http.client import HttpClient
 from asgard.math import round_up
 from asgard.models.agent import Agent
+
+_http_client = HttpClient()
 
 
 class MesosAgent(Agent):
@@ -42,35 +44,36 @@ class MesosAgent(Agent):
         self_address = f"http://{self.hostname}:{self.port}"
         containers_url = f"{self_address}/containers"
         apps = []
-        async with http_client.get(containers_url) as response:
-            data = await response.json()
-            all_apps: Set[str] = set()
-            for container_info in data:
-                app_id = MesosApp.transform_to_asgard_app_id(
-                    container_info["executor_id"]
-                )
-                if app_id not in all_apps:
-                    apps.append(MesosApp(**{"id": app_id}))
-                    all_apps.add(app_id)
-            return apps
+        response = await _http_client.get(containers_url)
+
+        data = await response.json()
+        all_apps: Set[str] = set()
+        for container_info in data:
+            app_id = MesosApp.transform_to_asgard_app_id(
+                container_info["executor_id"]
+            )
+            if app_id not in all_apps:
+                apps.append(MesosApp(**{"id": app_id}))
+                all_apps.add(app_id)
+        return apps
 
     async def tasks(self, app_id: str) -> List[MesosTask]:
         self_address = f"http://{self.hostname}:{self.port}"
         containers_url = f"{self_address}/containers"
-        async with http_client.get(containers_url) as response:
-            data = await response.json()
-            tasks_per_app: Dict[str, List[MesosTask]] = defaultdict(list)
-            for container_info in data:
-                app_id_ = MesosApp.transform_to_asgard_app_id(
-                    container_info["executor_id"]
+        response = await _http_client.get(containers_url)
+        data = await response.json()
+        tasks_per_app: Dict[str, List[MesosTask]] = defaultdict(list)
+        for container_info in data:
+            app_id_ = MesosApp.transform_to_asgard_app_id(
+                container_info["executor_id"]
+            )
+            tasks_per_app[app_id_].append(
+                MesosTask(
+                    **{
+                        "name": MesosTask.transform_to_asgard_task_id(
+                            container_info["executor_id"]
+                        )
+                    }
                 )
-                tasks_per_app[app_id_].append(
-                    MesosTask(
-                        **{
-                            "name": MesosTask.transform_to_asgard_task_id(
-                                container_info["executor_id"]
-                            )
-                        }
-                    )
-                )
-            return tasks_per_app[app_id]
+            )
+        return tasks_per_app[app_id]
