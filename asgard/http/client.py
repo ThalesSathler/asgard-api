e0556d836ed8ce795f77b6bd281b510719bea749
Dalton Barreto <daltonmatos@gmail.com>
2019-08-19 12:17:29 -0300

    Documentando HttpClient

--- asgard/http/client.py
+++ asgard/http/client.py
@@ -1,6 +1,6 @@
 from functools import wraps
 from http import HTTPStatus
-from typing import Optional, Dict, Any
+from typing import Dict, Any
 
 from aiohttp import ClientSession, ClientTimeout, ClientResponse  # type: ignore
 from aiohttp.client_exceptions import ClientResponseError
@@ -19,6 +19,16 @@ default_http_client_timeout = ClientTimeout(
 
 
 class HttpClient:
+    """
+    Wrapper em volta do objeto aiohttp.ClientSession. Possui a seguintes features:
+
+    - Já lança exception em caso de response ``4xx`` e ``5xx``. Esse comportamento pode ser sobrescrito, a cada request, passando o argumento ``raise_for_status=True``.
+    - Já segue redirect dos requests. 
+    - Headers que podem ser passados ao instanciar um objeto HttpClient
+        - Esses headers serão mesclados com quaisquer outros headers que forem passados no momento em que um request HTTP for feito.
+        - Em caso de headers de nomes iguais, os headers passados ao request terão maior precedência
+    - Timeout já configurado. Se nada for passado no construtor um timeout padrão já estará configurado. Esse timeout tem seus valores nas configs: :py:const:`asgard.conf.ASGARD_HTTP_CLIENT_TOTAL_TIMEOUT` e :py:const:`asgard.conf.ASGARD_HTTP_CLIENT_CONNECT_TIMEOUT`. Esses valores podem ser sobrescritos com as envs ``ASGARD_HTTP_CLIENT_TOTAL_TIMEOUT`` e ``ASGARD_HTTP_CLIENT_CONNECT_TIMEOUT``.
+    """
 
     _session: ClientSession
 
@@ -57,6 +67,10 @@ class HttpClient:
         raise_for_status: bool = True,
         **kwargs: Dict[str, Any],
     ) -> ClientResponse:
+        """
+        Método que é usado por todos os outros métodos para fazer um request.
+        O parametros recebidos por esse métodos definem os parametros recebidos pelo client de uma forma geral.
+        """
         try:
             resp = await self._session.request(
                 method,
@@ -79,18 +93,33 @@ class HttpClient:
         return resp
 
     async def get(self, url: str, **kwargs: Dict[str, Any]) -> ClientResponse:
+        """
+        Alias coroutine para GET <url>
+        """
         return await self._request("get", url, **kwargs)
 
     async def post(self, url: str, **kwargs: Dict[str, Any]) -> ClientResponse:
+        """
+        Alias coroutine para POST <url>
+        """
         return await self._request("post", url, **kwargs)
 
     async def put(self, url: str, **kwargs: Dict[str, Any]) -> ClientResponse:
+        """
+        Alias coroutine para PUT <url>
+        """
         return await self._request("put", url, **kwargs)
 
     async def patch(self, url: str, **kwargs: Dict[str, Any]) -> ClientResponse:
+        """
+        Alias coroutine para PATCH <url>
+        """
         return await self._request("patch", url, **kwargs)
 
     async def delete(
         self, url: str, **kwargs: Dict[str, Any]
     ) -> ClientResponse:
+        """
+        Alias coroutine para DELETE <url>
+        """
         return await self._request("delete", url, **kwargs)
